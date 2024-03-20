a11bde0844113f02e50197ff8e2570e9727d7790
Pablogs989 <pablogonsan2001@gmail.com>
2024-03-20 11:50:43 +0100

    Arrays

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Variables
+
 //alert("JavaScript funcionando correctamente");
 let variableSinValor;
 let booleano1 = true;
@@ -20,3 +22,22 @@ let variable = "Variable";
 let templateLiteral = `Esta es la varible: ${variable}`;
 console.log(templateLiteral);
 console.log(PI.toFixed(2));
+
+
+//Arrays
+
+const arrayVacio = [];
+const arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayNumeros2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const arrayNumerosPares = [0, 2, 4, 6, 8];
+const arrayNumerosNeg = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9];
+const holaMundo = ["Hola", "Mundo"];
+const loGuardoTodo = ["hola", "que", 23, 42.33, "tal"];
+const arrayDeArrays = [[756, 'nombre'], [225, 'apellido'], [298, 'direccion']];
+console.log(loGuardoTodo.length);
+const nuevo = "nuevo";
+loGuardoTodo.push(nuevo);
+console.log(loGuardoTodo);
+console.log(loGuardoTodo[1]);
+loGuardoTodo.push("Euralio");
+console.log(loGuardoTodo);
